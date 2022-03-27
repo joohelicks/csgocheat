702199571b567586c7130dde828697d4d15ed020
aj <email.email.email>
2022-03-27 14:05:12 +0300

    Automatically parse the address of client_client.so

--- definitions.h
+++ definitions.h
@@ -3,4 +3,3 @@
 
 #define dwLocalPlayer  0x22dd650
 #define m_flFlashDuration  0x10d90
-#define client_clientaddr 0x7fa9a3a00000 // change this value manually and compile
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/uio.h>
+#include <fstream>
 #include "definitions.h"
 
 void write_memory(pid_t pid, void* dst, void* src, size_t size)
@@ -23,10 +24,7 @@ void write_memory(pid_t pid, void* dst, void* src, size_t size)
     process_vm_writev(pid, &iosrc, 1, &iodst, 1, 0);
 }
 
-
-
-void read_memory(pid_t pid, void* src, void* dst, size_t size)
-{
+int read_memory(pid_t pid, void* src, void* dst, size_t size) {
     /*
     pid  = target process id
     src  = address to read from on the target process
@@ -41,17 +39,39 @@ void read_memory(pid_t pid, void* src, void* dst, size_t size)
     iosrc.iov_base = src;
     iosrc.iov_len  = size;
 
-    if (process_vm_readv(pid, &iodst, 1, &iosrc, 1, 0) == -1)
-		std::cout << "Error: " << strerror(errno) << std::endl;
+    if (process_vm_readv(pid, &iodst, 1, &iosrc, 1, 0) == -1) {
+		sleep(1);
+		return -1;
+	}
+	return 0;
 }
 
+uintptr_t parsemaps(uint32_t pid) {
+	if (pid == 0) {
+		return -1;
+	}
 
+	std::string so_address;
+	std::string path = "/proc/" + std::to_string(pid) + "/maps";
+	std::ifstream infile(path);
+	
+	if (infile.is_open()) {
+		std::string line;
+		while (std::getline(infile, line)) { 
+			if (line.find("client_client.so") != -1) { // find line with "client_client.so"
+				so_address = line.substr(0, line.find("-")); // from the same line, cut out the address of client_client.so
+				break;
+			}
+		}
+		infile.close();
+	} else {
+		return -1;
+	}
 
+	return std::stoul(so_address, 0, 16);
+}
 
-
-int main(int argc, char **argv)
-{
-
+int main(int argc, char **argv) {
 	if (argc < 2) {
 		std::cout << "usage: " << argv[0] << " <pid>" << std::endl;
 		return 0;
@@ -64,6 +84,7 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+	uintptr_t client_clientaddr = parsemaps(pid);
 	uintptr_t playeraddr = 0;
 
 	while (playeraddr == 0) {
@@ -75,16 +96,16 @@ int main(int argc, char **argv)
 	int flashdur = 0;
 	uintptr_t playerptr = *(&playeraddr);
 
-
 	while (true) {
-		read_memory(pid, (void*)(playerptr + m_flFlashDuration), &flashdur, sizeof(flashdur));
+		int retval = read_memory(pid, (void*)(playerptr + m_flFlashDuration), &flashdur, sizeof(flashdur));
+		if (retval == -1) {
+			std::cout << "Error: " << strerror(errno) << std::endl;
+			break;
+		}
 
 		if (flashdur > 0)
 			write_memory(pid, (void*)(playerptr + m_flFlashDuration), &zero, sizeof(zero));
 
 	}
-
     return 0;
 }
-
-
