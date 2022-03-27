fbe7f19cff16c703945dde9f37a121a8602b2250
aj <email.email.email>
2022-03-27 23:09:35 +0300

    reformatting code

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@
 
 #define ONGROUND (1 << 0)
 
+typedef struct offsets {
+	uintptr_t playerptr;
+	uintptr_t client_clientaddr;
+} offsets;
+
 void write_memory(pid_t pid, void* dst, void* src, size_t size)
 {
     /*
@@ -56,7 +61,6 @@ uintptr_t parsemaps(uint32_t pid) {
 		return -1;
 	}
 
-
 	std::string so_address;
 	std::string path = "/proc/" + std::to_string(pid) + "/maps";
 	std::ifstream infile(path);
@@ -78,68 +82,88 @@ uintptr_t parsemaps(uint32_t pid) {
 }
 constexpr uintptr_t dwForceJump = 0x2b86b50;
 
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		std::cout << "usage: " << argv[0] << " <pid>" << std::endl;
-		return 0;
+void bunnyhop(pid_t pid, offsets offsets) {
+	int zero = 0;
+	int bunnyhop = 6;
+	write_memory(pid, (void*)(offsets.client_clientaddr + dwForceJump), &bunnyhop, sizeof(bunnyhop));
+	usleep(100);
+	write_memory(pid, (void*)(offsets.client_clientaddr + dwForceJump), &zero, sizeof(zero));
+
+}
+
+bool should_bunnyhop(offsets offsets, int bhop, int flags) {
+	if (flags & (1 << 0) && bhop == true) {
+		return true;
 	}
+	return false;
+}
+
+void disable_flashbang(pid_t pid, offsets offsets) {
+	int zero = 0;
+	int flashdur = 0;
+	int retval = read_memory(pid, (void*)(offsets.playerptr + m_flFlashDuration), &flashdur, sizeof(flashdur));
+	if (retval == -1) {
+		std::cout << "Error: " << strerror(errno) << std::endl;
+		return;
+	}
+
+	if (flashdur > 0)
+		write_memory(pid, (void*)(offsets.playerptr + m_flFlashDuration), &zero, sizeof(zero));
+
+}
+
+int main(int argc, char **argv) {
+	pid_t pid;
+	int flags;
+	offsets offsets; 
 
 	Display *dpy =XOpenDisplay(0);
 	Window root = DefaultRootWindow(dpy);
 	XEvent ev;
-
 	int             keycode         = XKeysymToKeycode(dpy,XK_F1);
     Window          grab_window     = root;
     Bool            owner_events    = False;
     int             pointer_mode    = GrabModeAsync;
     int             keyboard_mode   = GrabModeAsync;
 
-	XGrabKey(dpy, keycode, 0, grab_window, owner_events, pointer_mode,
-			keyboard_mode);
-	XSelectInput(dpy, root, KeyPressMask);
+	if (argc < 2) {
+		std::cout << "usage: " << argv[0] << " <pid>" << std::endl;
+		return 0;
+	}
 
-	uint32_t pid = atoi(argv[1]);
-
+	pid = atoi(argv[1]);
 	if (pid == 0) {
 		std::cout << "Please give proper PID" << std::endl;
 		return 0;
 	}
 
-	uintptr_t client_clientaddr = parsemaps(pid);
+	XGrabKey(dpy, keycode, 0, grab_window, owner_events, pointer_mode,
+			keyboard_mode);
+	XSelectInput(dpy, root, KeyPressMask);
+
+	offsets.client_clientaddr = parsemaps(pid);
 	uintptr_t playeraddr = 0;
 
 	while (playeraddr == 0) {
 		sleep(1);
-		read_memory(pid, (void*)(client_clientaddr+dwLocalPlayer), &playeraddr, sizeof(playeraddr));
+		read_memory(pid, (void*)(offsets.client_clientaddr+dwLocalPlayer), &playeraddr, sizeof(playeraddr));
 	}
 
+	offsets.playerptr = *(&playeraddr);
+	flags=0;
 
-	int zero = 0;
-	int flashdur = 0;
-	int bunnyhop = 6;
-	uintptr_t playerptr = *(&playeraddr);
-	int flags=0;
 	bool bhop = false;
 
 	while (true) {
-		int retval = read_memory(pid, (void*)(playerptr + m_flFlashDuration), &flashdur, sizeof(flashdur));
-		if (retval == -1) {
-			std::cout << "Error: " << strerror(errno) << std::endl;
-			break;
-		}
+		disable_flashbang(pid, offsets);
+		
+		read_memory(pid, (void*)(offsets.playerptr + m_fFlags), &flags, sizeof(flags));
 
-		if (flashdur > 0)
-			write_memory(pid, (void*)(playerptr + m_flFlashDuration), &zero, sizeof(zero));
-
-		read_memory(pid, (void*)(playerptr + m_fFlags), &flags, sizeof(flags));
 		if (XCheckWindowEvent(dpy, root, KeyPressMask, &ev))
 			bhop = !bhop;
 
-		if (flags & (1 << 0) && bhop == true) {
-			write_memory(pid, (void*)(client_clientaddr + dwForceJump), &bunnyhop, sizeof(bunnyhop));
-			usleep(100);
-			write_memory(pid, (void*)(client_clientaddr + dwForceJump), &zero, sizeof(zero));
-		}
+		if (should_bunnyhop(offsets, bhop, flags))
+			bunnyhop(pid, offsets);
 	}
 	XCloseDisplay(dpy);
     return 0;
