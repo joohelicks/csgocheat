0c7e6838ca24a3b9f0c84aae1b69eb09ae3f6ca9
aj <email.email.email>
2022-03-27 21:39:01 +0300

    bunnyhop and reading input with Xlib

--- definitions.h
+++ definitions.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
 
+#define m_fFlags  0x13c
 #define dwLocalPlayer  0x22dd650
 #define m_flFlashDuration  0x10d90
--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@
 #include <unistd.h>
 #include <sys/uio.h>
 #include <fstream>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
 #include "definitions.h"
+
+#define ONGROUND (1 << 0)
 
 void write_memory(pid_t pid, void* dst, void* src, size_t size)
 {
@@ -51,6 +56,7 @@ uintptr_t parsemaps(uint32_t pid) {
 		return -1;
 	}
 
+
 	std::string so_address;
 	std::string path = "/proc/" + std::to_string(pid) + "/maps";
 	std::ifstream infile(path);
@@ -70,12 +76,27 @@ uintptr_t parsemaps(uint32_t pid) {
 
 	return std::stoul(so_address, 0, 16);
 }
+constexpr uintptr_t dwForceJump = 0x2b86b50;
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		std::cout << "usage: " << argv[0] << " <pid>" << std::endl;
 		return 0;
 	}
+
+	Display *dpy =XOpenDisplay(0);
+	Window root = DefaultRootWindow(dpy);
+	XEvent ev;
+
+	int             keycode         = XKeysymToKeycode(dpy,XK_F1);
+    Window          grab_window     = root;
+    Bool            owner_events    = False;
+    int             pointer_mode    = GrabModeAsync;
+    int             keyboard_mode   = GrabModeAsync;
+
+	XGrabKey(dpy, keycode, 0, grab_window, owner_events, pointer_mode,
+			keyboard_mode);
+	XSelectInput(dpy, root, KeyPressMask);
 
 	uint32_t pid = atoi(argv[1]);
 
@@ -92,9 +113,13 @@ int main(int argc, char **argv) {
 		read_memory(pid, (void*)(client_clientaddr+dwLocalPlayer), &playeraddr, sizeof(playeraddr));
 	}
 
+
 	int zero = 0;
 	int flashdur = 0;
+	int bunnyhop = 6;
 	uintptr_t playerptr = *(&playeraddr);
+	int flags=0;
+	bool bhop = false;
 
 	while (true) {
 		int retval = read_memory(pid, (void*)(playerptr + m_flFlashDuration), &flashdur, sizeof(flashdur));
@@ -106,6 +131,16 @@ int main(int argc, char **argv) {
 		if (flashdur > 0)
 			write_memory(pid, (void*)(playerptr + m_flFlashDuration), &zero, sizeof(zero));
 
+		read_memory(pid, (void*)(playerptr + m_fFlags), &flags, sizeof(flags));
+		if (XCheckWindowEvent(dpy, root, KeyPressMask, &ev))
+			bhop = !bhop;
+
+		if (flags & (1 << 0) && bhop == true) {
+			write_memory(pid, (void*)(client_clientaddr + dwForceJump), &bunnyhop, sizeof(bunnyhop));
+			usleep(100);
+			write_memory(pid, (void*)(client_clientaddr + dwForceJump), &zero, sizeof(zero));
+		}
 	}
+	XCloseDisplay(dpy);
     return 0;
 }
